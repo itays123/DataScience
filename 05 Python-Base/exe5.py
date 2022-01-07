f92eedefb9ef51a6c1547055d017c5d0e525697b
Itay Schechner <itays2005@gmail.com>
2022-01-07 11:19:53 +0200

    FINISH: Exercise 5

---
+++ 05 Python-Base/exe5.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create matrix
+matrix = np.ones((5, 5))
+print(matrix)
+
+maxRowCol = len(matrix) - 1
+matrix = matrix * 0
+matrix[0] = 1
+matrix[maxRowCol] = 1
+matrix[:,0] = 1
+matrix[:,maxRowCol] = 1
+print(matrix)
+
+
