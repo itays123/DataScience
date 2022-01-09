d47a9e00a5caeacc8d3173d78d9e671e4f506915
itays123 <itays2005@gmail.com>
2022-01-09 17:17:38 +0200

    FIX: Exercise 8

--- 05 Python-Base/exe8.py
+++ 05 Python-Base/exe8.py
@@ -13,9 +13,5 @@ print(merge1)
 merge2 = np.array((merge1[0], merge1[1], merge1[2]))
 print(merge2)
 
-arr1 = np.split(x1, 2);
-arr2 = np.split(x2, 2);
-arr3 = np.split(x3, 2);
-merge3 = np.concatenate((arr1, arr2, arr3))
-merge3 = np.array_split(merge3, 2)
-print(np.array(merge3))
+merge.shape = (2, 3, 3);
+print(merge)
