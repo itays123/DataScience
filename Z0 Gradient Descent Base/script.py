a3ac8e1bf885a78cf35bbd3cfeba6c29f970f9ef
itays123 <itays2005@gmail.com>
2022-01-13 17:23:24 +0200

    Rename: Rename stuff

--- Z0 Gradient Descent Base/script.py
+++ Z0 Gradient Descent Base/script.py
@@ -3,7 +3,7 @@ import numpy as np
 x = np.arange(0, 9, 1)
 y = np.array([1,4,2,5,7,8,8,9,10])
 
-def GradientDescendent(x,y,learning_rate = 0.01, epochs = 1000):
+def GradientDescent(x,y,learning_rate = 0.01, epochs = 1000):
     m = 0
     b = 0
     for _ in range(epochs):
