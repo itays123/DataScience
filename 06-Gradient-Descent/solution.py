3844f6b49946287add797b1d258bfb6461ade718
itays123 <itays2005@gmail.com>
2022-01-13 19:21:29 +0200

    FIX: Program works

--- 06-Gradient-Descent/solution.py
+++ 06-Gradient-Descent/solution.py
@@ -6,14 +6,14 @@ advertising=dataset[:,1]
 sales=dataset[:,2]
 
 # Divide sales by constant for easier calculations, and consier it when giving new predictions
-MILLIONS_IN_BILLION=1000
-sales /= MILLIONS_IN_BILLION
+CONSTANT=100
+sales /= CONSTANT
+advertising /= CONSTANT
 
 # Core functionality
 def GradientDescent(x,y,learning_rate = 0.01, epochs = 1000):
     m = 0
     b = 0
-    print("x y guess error new_m, new_b")
     for _ in range(epochs):
         for i in range(len(x)):
             xi = x[i]
@@ -22,18 +22,14 @@ def GradientDescent(x,y,learning_rate = 0.01, epochs = 1000):
             error = guess - yi
             m = m - (error * xi) * learning_rate
             b = b - error * learning_rate
-            print(xi, yi, guess, error, m, b)
     return m, b
 
 # Calculate the cost
 def CalculateCost(x,y,m,b):
-    sumOfErrorsSquared = 0
-    for i in range(len(x)):
-        error = m * x[i] + b - y[i]
-        sumOfErrorsSquared += (error **2)
-    return sumOfErrorsSquared
+    errors = m * x + b - y
+    return np.sum(errors ** 2)
 
 # Calculate the effect of the advertisement budget over the sales
-m, b = GradientDescent(advertising, sales)
+m, b = GradientDescent(advertising, sales, 0.1, 100)
 print("m:", m, "b:", b, "COST:", CalculateCost(advertising, sales, m, b))
-print("With one more million euros inversted, sales are expected to grow by " + str(m * MILLIONS_IN_BILLION) + " million euros")
+print("With one more million euros inversted, sales are expected to grow by " + str(m) + " million euros")
