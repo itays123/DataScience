875f298ea8a3302749b2406735d9382f5b14bef0
Itay Schechner <itays2005@gmail.com>
2022-03-07 15:32:03 +0200

    FEATURE: Vector distance

--- 09-KNN/distance.py
+++ 09-KNN/distance.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 # Calculate distance between two points
 def distance(p1: Tuple[float, float], p2: Tuple[float, float]):
@@ -12,6 +13,17 @@ def readPoint():
     y = float(input("Enter y value: "))
     return x, y
 
-p1 = readPoint()
-p2 = readPoint()
-print("Distance: ", distance(p1, p2))
+# p1 = readPoint()
+# p2 = readPoint()
+# print("Distance: ", distance(p1, p2))
+
+# Calculate the distance between two multi-dimensional points
+def distanceVec(p1: np.array, p2: np.array):
+    distanceVec = p1 - p2
+    return math.sqrt(np.dot(distanceVec, distanceVec))
+
+
+p1 = np.array([0, 0, 0])
+p2 = np.array([2, 2, 1])
+
+print("Distance: ", distanceVec(p1, p2))
